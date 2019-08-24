eae4adff3f22935d381d7d654a121f12b3d88be2
Tomás Migone <tomasmigone@gmail.com>
2019-08-24 12:09:33 -0300

    first commit

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,62 @@
+<template>
+    <footer class="footer has-cards">
+
+        <div class="container">
+
+            <hr>
+            <div class="row align-items-center justify-content-md-between">
+                <div class="col-md-6">
+                    <div class="copyright">
+                        &copy; {{year}}
+                        Little Robots
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <ul class="nav nav-footer justify-content-end">
+
+                        <!-- <li class="nav-item" style="padding: 0 2.5px 0 2.5px;">
+                    <a target="_blank" rel="noopener" href="https://twitter.com/creativetim"
+                       class="btn btn-neutral btn-icon-only btn-twitter btn-round btn-lg" data-toggle="tooltip"
+                       data-original-title="Follow us">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                        </li> -->
+                        <li class="nav-item" style="padding: 0 2.5px 0 2.5px;">
+                    <a target="_blank" rel="noopener" href="https://www.facebook.com/creativetim"
+                       class="btn btn-neutral btn-icon-only btn-instagram btn-round btn-lg" data-toggle="tooltip"
+                       data-original-title="Like us">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+                        </li>
+                        <!-- <li class="nav-item" style="padding: 0 2.5px 0 2.5px;">
+                    <a target="_blank" rel="noopener" href="https://dribbble.com/creativetim"
+                       class="btn btn-neutral btn-icon-only btn-dribbble btn-lg btn-round" data-toggle="tooltip"
+                       data-original-title="Follow us">
+                        <i class="fa fa-dribbble"></i>
+                    </a>
+                        </li> -->
+                        <li class="nav-item" style="padding: 0 2.5px 0 2.5px;">
+                    <a target="_blank" rel="noopener" href="https://github.com/creativetimofficial"
+                       class="btn btn-neutral btn-icon-only btn-github btn-round btn-lg" data-toggle="tooltip"
+                       data-original-title="Star on Github">
+                        <i class="fa fa-github"></i>
+                    </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+  name: 'app-footer',
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+};
+</script>
+<style>
+</style>
---
+++ src/views/Landing.vue
@@ -0,0 +1,321 @@
+<template>
+<!-- eslint-disable -->
+    <div>
+
+        <div class="position-relative">
+            <!-- shape Hero -->
+            <section class="section-shaped my-0">
+                <!-- <div class="shape shape-style-1 shape-default shape-skew" style="background-image: url('img/board.png'); background-size: cover;"> -->
+                <div class="shape shape-style-1 shape-default shape-skew">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="container shape-container d-flex">
+                    <div class="col px-0">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h1 class="display-3  text-white">Cursos de robótica
+                                    <span>para principiantes</span>
+                                </h1>
+                                <p class="lead  text-white">Aprendé a trabajar sobre las plataformas de hardware y software más populares de la actualidad.</p>
+                                <!-- <div class="btn-wrapper">
+                                    <base-button tag="a"
+                                                 href="https://demos.creative-tim.com/argon-design-system/docs/components/alerts.html"
+                                                 class="mb-3 mb-sm-0"
+                                                 type="info"
+                                                 icon="fa fa-code">
+                                        Conocé más
+                                    </base-button>
+                                    <base-button tag="a"
+                                                 href="https://www.creative-tim.com/product/argon-design-system"
+                                                 class="mb-3 mb-sm-0"
+                                                 type="white"
+                                                 icon="ni ni-cloud-download-95">
+                                        Quiero anotarme
+                                    </base-button>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- 1st Hero Variation -->
+        </div>
+        <section class="section section-lg pt-lg-0 mt--200">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="row row-grid">
+                            <div class="col-lg-4">
+                                    <img src="@/assets/breadboard.jpg" class="img-fluid" alt="">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <!-- <icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
+                                    </icon> -->
+                                    <h6 class="text-primary text-uppercase">Introducción a la robótica</h6>
+                                    <p class="description mt-3">Aprendé los concéptos básicos de la programación y la electrónica mediante al uso de la plataforma de desarrollo más popular de la actualidad: Arduino.</p>
+                                    <p class="description mt-3">Integra tus conocimientos trabajando con proyectos de robótica simples pero desafiantes.</p>
+                                    <div>
+                                        <badge type="primary" rounded>nivel: inicial</badge>
+                                        <badge type="primary" rounded>arduino</badge>
+                                        <badge type="primary" rounded>c++</badge>
+                                    </div>
+                                    <base-button tag="a" href="#curso-inicial" type="primary" class="mt-4">
+                                        Ver más
+                                    </base-button>
+                                </card>
+                            </div>
+                            <div class="col-lg-4">
+                                    <img src="@/assets/car.jpg" class="img-fluid" alt="">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <h6 class="text-success text-uppercase">Curso intermedio de Robótica</h6>
+                                    <p class="description mt-3">Aprendé sobre funcionalidades avanzadas de Arduino y programación.</p>
+                                    <p class="description mt-3">Trabajá con proyectos más complejos con un mayór foco en la parte mecánica.</p>
+                                    <div>
+                                        <badge type="success" rounded>nivel: intermedio</badge>
+                                        <badge type="success" rounded>arduino</badge>
+                                        <!-- <badge type="success" rounded>success</badge> -->
+                                    </div>
+                                    <base-button disabled type="success" class="mt-4">
+                                        Próximamente
+                                    </base-button>                                    
+                                </card>
+                            </div>
+                            <div class="col-lg-4">
+                                    <img src="@/assets/rpi.jpg" class="img-fluid" alt="">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <h6 class="text-warning text-uppercase">Curso avanzado de Robótica</h6>
+                                    <p class="description mt-3">Amplia tus posibilidades aprendiendo sobre la microcomputadora Raspberry Pi.</p>
+                                    <p class="description mt-3">Aprendé sobre domótica y Internet of Things (IoT). Integrá tus proyectos con otros sistemas en internet.</p>
+                                    <div>
+                                        <badge type="warning" rounded>nivel: avanzado</badge>
+                                        <badge type="warning" rounded>Raspberry Pi</badge>
+                                    </div>
+                                    <base-button disabled type="warning" class="mt-4">
+                                        Próximamente
+                                    </base-button>     
+                                </card>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section section section-shaped my-0 overflow-hidden">
+            <div class="shape shape-style-1 bg-gradient-warning shape-skew">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="container py-0">
+                <div class="row row-grid align-items-center">
+                    <div class="col-md-6 order-lg-2 ml-lg-auto">
+                        <div class="position-relative pl-md-5">
+                            <img src="img/ill/ill-2.svg" class="img-center img-fluid">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="d-flex px-3">
+                            <!-- <div>
+                                <icon name="ni ni-building" size="lg" class="bg-gradient-white" color="primary" shadow
+                                      rounded></icon>
+                            </div> -->
+                            <div class="pl-4">
+                                <h4 class="display-3 text-white">Nuestra propuesta</h4>
+                                <p class="text-white">Creemos que la mejor forma de aprender es haciendo. Por eso ofrecemos una propuesta integral que combina el trabajo en clase con asistencia continua durante la cursada.</p>
+                            </div>
+                        </div>
+                        <card shadow class="shadow-lg--hover mt-5">
+                            <div class="d-flex px-3">
+                                <div>
+                                    <icon name="ni ni-building" gradient="success" color="white" shadow
+                                          rounded></icon>
+                                </div>
+                                <div class="pl-4">
+                                    <h5 class="title text-success">Cursos presenciales</h5>
+                                    <p>Dictamos los cursos de manera presencial en nuestras aulas ubicadas en Beccar, San Isidro. Contamos con todos los materiales necesarios, solamente necesitas una notebook.</p>
+                                </div>
+                            </div>
+                        </card>
+                        <card shadow class="shadow-lg--hover mt-5">
+                            <div class="d-flex px-3">
+                                <div>
+                                    <icon name="ni ni-badge" gradient="warning" color="white" shadow
+                                          rounded></icon>
+                                </div>
+                                <div class="pl-4">
+                                    <h5 class="title text-warning">Consultas online</h5>
+                                    <p>Los alumnos tienen acceso a un canal de comunicación privado donde los acompañamos con cualquier consulta o inquietud que tengan fuera de clases.</p>
+                                </div>
+                            </div>
+                        </card>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section section-lg">
+            <div class="container">
+                <div class="row justify-content-center text-center mb-lg">
+                    <div class="col-lg-8">
+                        <h2 class="display-3">Nosotros</h2>
+                        <p class="lead text-muted">Contamos con años de experiencia profesional en la industria del software y la electrónica. Hace cuatro años dictamos diversos cursos de programación, robótica, matemática y física para alumnos de nivel secundario.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-lg-6 mb-5 mb-lg-0">
+                        <div class="px-4">
+                            <img v-lazy="'img/theme/nitt.jpg'"
+                                 class="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                                 style="width: 200px;">
+                            <div class="pt-4 text-center">
+                                <h5 class="title">
+                                    <span class="d-block mb-1">Juan</span>
+                                    <small class="h6 text-muted">Ing. Informático</small>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 mb-5 mb-lg-0">
+                        <div class="px-4">
+                            <img v-lazy="'img/theme/tomi.jpeg'"
+                                 class="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                                 style="width: 200px;">
+                            <div class="pt-4 text-center">
+                                <h5 class="title">
+                                    <span class="d-block mb-1">Tomi</span>
+                                    <small class="h6 text-muted">Ing. Electrónico</small>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                        <div class="px-4">
+                            <img v-lazy="'img/theme/team-3-800x800.jpg'"
+                                 class="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                                 style="width: 200px;">
+                            <div class="pt-4 text-center">
+                                <h5 class="title">
+                                    <span class="d-block mb-1">Alexander Smith</span>
+                                    <small class="h6 text-muted">UI/UX Designer</small>
+                                </h5>
+                                <div class="mt-3">
+                                    <base-button tag="a" href="#" type="info" icon="fa fa-twitter" rounded
+                                                 icon-only></base-button>
+                                    <base-button tag="a" href="#" type="info" icon="fa fa-facebook" rounded
+                                                 icon-only></base-button>
+                                    <base-button tag="a" href="#" type="info" icon="fa fa-dribbble" rounded
+                                                 icon-only></base-button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                        <div class="px-4">
+                            <img v-lazy="'img/theme/team-4-800x800.jpg'"
+                                 class="rounded-circle img-center img-fluid shadow shadow-lg--hover"
+                                 style="width: 200px;">
+                            <div class="pt-4 text-center">
+                                <h5 class="title">
+                                    <span class="d-block mb-1">John Doe</span>
+                                    <small class="h6 text-muted">Founder and CEO</small>
+                                </h5>
+                                <div class="mt-3">
+                                    <base-button tag="a" href="#" type="success" icon="fa fa-twitter" rounded
+                                                 icon-only></base-button>
+                                    <base-button tag="a" href="#" type="success" icon="fa fa-facebook" rounded
+                                                 icon-only></base-button>
+                                    <base-button tag="a" href="#" type="success" icon="fa fa-dribbble" rounded
+                                                 icon-only></base-button>
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </section>
+        <section class="section section-shaped my-0 overflow-hidden">
+            <div class="shape shape-style-3 bg-gradient-default shape-skew">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="container pt-lg pb-300">
+                <div class="row text-center justify-content-center">
+                    <div class="col-lg-10">
+                        <h2 class="display-3 text-white">Contactanos</h2>
+                        <p class="lead text-white">Si estás interesado en alguno de los cursos o querés hacernos una consulta, contactanos!</p>
+                    </div>
+                </div>
+                <!-- <div class="row row-grid mt-5">
+                    <div class="col-lg-4">
+                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
+                        <h5 class="text-white mt-3">Building tools</h5>
+                        <p class="text-white mt-3">Some quick example text to build on the card title and make up the
+                            bulk of the card's content.</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <icon name="ni ni-ruler-pencil" size="lg" gradient="white" shadow round color="primary"></icon>
+                        <h5 class="text-white mt-3">Grow your market</h5>
+                        <p class="text-white mt-3">Some quick example text to build on the card title and make up the
+                            bulk of the card's content.</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <icon name="ni ni-atom" size="lg" gradient="white" shadow round color="primary"></icon>
+                        <h5 class="text-white mt-3">Launch time</h5>
+                        <p class="text-white mt-3">Some quick example text to build on the card title and make up the
+                            bulk of the card's content.</p>
+                    </div>
+                </div> -->
+            </div>
+        </section>
+        <section class="section section-lg pt-lg-0 section-contact-us">
+            <div class="container">
+                <div class="row justify-content-center mt--300">
+                    <div class="col-lg-8">
+                        <card gradient="secondary" shadow body-classes="p-lg-5">
+                            <h4 class="mb-1">Dejanos tus datos de contacto</h4>
+                            <!-- <p class="mt-0">Dejanos tus datos</p> -->
+                            <base-input class="mt-5"
+                                        alternative
+                                        placeholder="Nombre"
+                                        addon-left-icon="ni ni-user-run">
+                            </base-input>
+                            <base-input alternative
+                                        placeholder="Email"
+                                        addon-left-icon="ni ni-email-83">
+                            </base-input>
+                            <base-input alternative
+                                        placeholder="Teléfono"
+                                        addon-left-icon="ni ni-bell-55">
+                            </base-input>
+                            <base-input class="mb-4">
+                                    <textarea class="form-control form-control-alternative" name="name" rows="4"
+                                              cols="80" placeholder="Tu consulta..."></textarea>
+                            </base-input>
+                            <base-button type="default" round block size="lg">
+                                Enviar
+                            </base-button>
+                        </card>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {}
+};
+</script>
